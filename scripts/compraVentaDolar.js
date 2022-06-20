f018b9c5423db3a462b1e81c5daf4a9c3367df78
marie177 <gottigmarianella@hotmail.com>
2022-06-19 22:38:30 -0300

    Agrego funcion para obtener cotizacion actual en Seccion compra y venta

--- scripts/compraVentaDolar.js
+++ scripts/compraVentaDolar.js
@@ -1,17 +1,15 @@
-const obtenerValor = (bool) => { // si recibe 0 es venta o 1 compra
+const obtenerValor = (tipoAccion) => { // si recibe 0 es venta o 1 compra
    
     var valorIngresado= parseFloat(document.getElementById("valor").value);
     var valorDolar;
     var txtValorFinal =document.getElementById("valorFinal");
     var valorFinal;
-    var tipoAccion = bool;
     let url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
 
     fetch(url)
         .then(response => response.json())
         .then(respuestaDecodificada => {
-            console.log('entro')
-            if(!tipoAccion){
+            if(tipoAccion == 0){
                 valorDolar = parseFloat(respuestaDecodificada[1]['casa']['venta']);
                 console.log(valorDolar)
                 valorFinal = valorIngresado * valorDolar;
@@ -19,13 +17,37 @@ const obtenerValor = (bool) => { // si recibe 0 es venta o 1 compra
                 console.log(valorFinal)
                 txtValorFinal.textContent = `Son  $${valorFinal}`
             }
-            else if(tipoAccion){
+            else if(tipoAccion == 1){
                 valorDolar = parseFloat(respuestaDecodificada[1]['casa']['compra']);
                 console.log(valorDolar)
                 valorFinal = valorIngresado * valorDolar;
 
                 console.log(valorFinal)
                 txtValorFinal.textContent = `Son  $${valorFinal}`
+            }
+
+        })
+        .catch(error => console.log(error))
+}
+
+const obtenerCotizacionActual = (tipoAccion) =>{ // si recibe 0 es venta o 1 compra
+
+    var cotizacionActualP= document.getElementById("valorCotizacionActual");
+
+    let url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+
+    fetch(url)
+        .then(response => response.json())
+        .then(respuestaDecodificada => {
+            if(tipoAccion == 0){
+                valorDolar = respuestaDecodificada[1]['casa']['venta'];
+                console.log(valorDolar)
+                cotizacionActualP.textContent = `Cotizacion actual: 1 u$s = ${valorDolar}`
+            }
+            else if(tipoAccion ==1 ){
+                valorDolar = respuestaDecodificada[1]['casa']['compra'];
+                console.log(valorDolar)
+                cotizacionActualP.textContent = `Cotizacion actual: 1 u$s = ${valorDolar}`
             }
 
         })
